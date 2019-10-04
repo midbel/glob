59f3486362cacfdf47a0a42810326344851d9cc6
nico <brun.nicolas21@gmail.com>
2019-10-04 12:25:37 +0200

    initial commit

---
+++ glob.go
@@ -0,0 +1,29 @@
+package glob
+
+const (
+  slash = '/'
+  backslash = '\\'
+)
+
+const (
+  star = '*'
+  mark = '?'
+  any  = "**"
+)
+
+func Walk(pattern string) ([]string, error) {
+  return nil, nil
+}
+
+func Glob(dir, pattern string) ([]string, error) {
+  parts := strings.FieldsFunc(pattern, func(r rune) bool { return r == slash || r == backslash })
+  return glob(dir, parts)
+}
+
+func Match(dir, pattern string) bool {
+  return false
+}
+
+func glob(dir, parts []string) ([]string, error) {
+  return nil, nil
+}
