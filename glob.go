f6a061ae9739d5c4fbb4568274d88d3ff7d62868
nico <brun.nicolas21@gmail.com>
2019-10-04 14:40:09 +0200

    implements Match

--- glob.go
+++ glob.go
@@ -1,7 +1,7 @@
 package glob
 
 import (
-  "strings"
+	"strings"
 )
 
 const (
@@ -25,9 +25,43 @@ func Glob(dir, pattern string) ([]string, error) {
 }
 
 func Match(dir, pattern string) bool {
-	return false
+	return match(dir, pattern)
 }
 
 func glob(dir string, parts []string) ([]string, error) {
 	return nil, nil
+}
+
+func match(str, pat string) bool {
+	// shortcut: pat is only one star or pat and str are identicals
+	if pat == string(star) || (len(str) == len(pat) && str == pat) {
+		return true
+	}
+	var i, j int
+	// for ; i < len(pat) && j < len(str); i++ {
+	for ; i < len(pat); i++ {
+		switch char := pat[i]; {
+		case char == star:
+			// multiple stars is the same as one star
+			for i = i + 1; i < len(pat) && pat[i] == char; i++ {
+			}
+			// trailing star matchs rest of text
+			if i >= len(pat) {
+				return true
+			}
+			for j < len(str) {
+				if ok := match(str[j:], pat[i:]); ok {
+					return ok
+				}
+				j++
+			}
+		case char == mark || (j < len(str) && char == str[j]):
+			// default
+		default:
+			return false
+		}
+		j++
+	}
+	// match when all characters of pattern and text have been read
+	return i == len(pat) && j == len(str)
 }
