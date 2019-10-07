2298bcf5414f85103f6253301d20baac2cdee375
midbel <brun.nicolas21@gmail.com>
2019-10-07 19:55:55 +0200

    remove unimplemented func

--- glob.go
+++ glob.go
@@ -1,9 +1,5 @@
 package glob
 
-import (
-	"strings"
-)
-
 const (
 	slash     = '/'
 	backslash = '\\'
@@ -15,21 +11,8 @@ const (
 	any  = "**"
 )
 
-func Walk(pattern string) ([]string, error) {
-	return nil, nil
-}
-
-func Glob(dir, pattern string) ([]string, error) {
-	parts := strings.FieldsFunc(pattern, func(r rune) bool { return r == slash || r == backslash })
-	return glob(dir, parts)
-}
-
 func Match(dir, pattern string) bool {
 	return match(dir, pattern)
-}
-
-func glob(dir string, parts []string) ([]string, error) {
-	return nil, nil
 }
 
 func match(str, pat string) bool {
