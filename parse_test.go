0b4c41b52077cd4c4c79a76028d2ba9794cedd8e
midbel <brun.nicolas21@gmail.com>
2019-12-20 22:16:09 +0100

    skeleton to test Compile

---
+++ parse_test.go
@@ -0,0 +1,21 @@
+package glob
+
+import (
+  "testing"
+)
+
+func TestCompile(t *testing.T) {
+  data := []struct{
+    Pattern string
+    Fail bool
+  }{}
+  for i, d := range data {
+    _, err := Compile(d.Pattern)
+    if d.Fail == false && err != nil {
+      t.Errorf("%d) compile fail %q: %v", d.Pattern, err)
+    }
+    if d.Fail == true && err == nil {
+      t.Errorf("%d) compile fail %q", d.Pattern)
+    }
+  }
+}
